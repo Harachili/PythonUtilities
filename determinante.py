12432d1d11949f792a8a5469157adf326196bb25
Harachili <55925983+Harachili@users.noreply.github.com>
2020-12-22 22:04:48 +0100

    Update determinante.py
    This simple Python program takes a square matrix in input and it checks if the matrix is actually square; if so, the program calculates recursively the determinant of the matrix.
    Algorithm
    
    The program implements the Laplace expansion algorithm, which can be easily expressed recursively.
    First of all it checks if the matrix is square, then as a base case controls if the matrix is already 2x2 or 1x1.
    If it is, the resolution is immediate and in a matrix like:
      [[a, b],
       [c, d]]
    will just return (a*d) - (d*b)
    else if the matrix is bigger, it will just recursively calculate the Laplace algorithm which is:
    [= \sum_{j=1}^{n} (-1)^{1+j}a_{1j}det(A_{1,j})]

--- determinante.py
+++ determinante.py
@@ -6,14 +6,18 @@ Created on Tue Dec 15 13:24:00 2020
 """
 #CALCOLO DETERMINANTE DI UNA MATRICE
 
-scalaMatrice = lambda m, i, j: [row[:j] + row[j+1:] for row in (m[:i]+m[i+1:])]
+scalaMatrice = lambda m, i, j: [row[:j] + row[j+1:] for row in (m[:i]+m[i+1:])] # This lambda 
 def trovaDeterminante(m):
-    if len(m) == 1: return m[0][0]
+    if len(m) != len(m[0]):
+        raise Exception("Sorry, your matrix has to be a square one!")
+    if len(m) == 1: 
+        return m[0][0]
     if len(m) == 2:
         return m[0][0]*m[1][1]-m[0][1]*m[1][0]
-    
+
     determinante = 0
     for c in range(len(m)):
         segno = (-1)**c
         determinante += segno*m[0][c]*trovaDeterminante(scalaMatrice(m,0,c))
     return determinante
+    
